dce44f79ea8764821386f276ca1ef96d8ef94141
dengxin <dengxin@juxinli.com>
2019-07-12 17:33:33 +0800

    修改部署提示

--- src/main/resources/static/js/controller/Deploy.js
+++ src/main/resources/static/js/controller/Deploy.js
@@ -1,7 +1,10 @@
 app.controller("DeployController", function($scope, $http) {
+    $scope.deploying = false;
 
     // 上传部署zip包
     $scope.deploy = function (){
+        $scope.deploying = true;
+        $("#uploadBtn").attr("disabled", "disabled");
         $scope.st = "";
         var formData = new FormData();
         formData.append('file', document.querySelector('input[type=file]').files[0]);
@@ -15,6 +18,8 @@ app.controller("DeployController", function($scope, $http) {
             headers: {'Content-Type': undefined},
             transformRequest: angular.identity
         }).then(function (response) {
+            $("#uploadBtn").removeAttr("disabled");
+            $scope.deploying = false;
             // 获取zip包的上传后状态
             // console.log(response.data.note);
             // console.log(response.data.code);
